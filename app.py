97c18a38b687712708fc5dc63619f845e79fb4b2
kad-del <naikkadambini1@gmail.com>
2025-12-31 17:03:00 +0530

    app.py

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
 import streamlit as st
 import joblib
-import pandas as pd
+import numpy as np
 
-# Load trained model
+# Load model
 model = joblib.load("risk_model.pkl")
 
-# App title
-st.title("SAFEROUTE AI - Crowd & Traffic Risk Prediction 🚦")
+# Title
+st.title("SAFEROUTE AI-Crowd & Traffic Risk Prediction 🚦")
 
-# User inputs
+# Inputs
 day = st.selectbox("Day Type", ["Weekday", "Weekend"])
 time = st.selectbox("Time of Day", ["Morning", "Afternoon", "Evening"])
 traffic = st.selectbox("Traffic Level", ["Low", "Medium", "High"])
 
-# Encode inputs (VERY IMPORTANT)
+# Encoding
 day_val = 0 if day == "Weekday" else 1
 time_val = {"Morning": 0, "Afternoon": 1, "Evening": 2}[time]
 traffic_val = {"Low": 0, "Medium": 1, "High": 2}[traffic]
 
-# Prediction button
+# Prediction
 if st.button("Predict Risk"):
-   import numpy as np
-
-input_data = np.array([[day_val, time_val, traffic_val]])
-prediction = model.predict(input_data)[0]
-
+    input_data = np.array([[day_val, time_val, traffic_val]])
     prediction = model.predict(input_data)[0]
     st.success(f"Predicted Risk Level: {prediction}")
